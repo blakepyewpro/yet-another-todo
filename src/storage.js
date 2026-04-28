42241c0369aa642aafc8eceb6aaaa6443d5d5b44
blakepyewpro <148835335+blakepyewpro@users.noreply.github.com>
2026-04-27 23:59:47 -0400

    feat: added storage class

---
+++ src/storage.js
@@ -0,0 +1,34 @@
+export default class Storage {
+
+  static storageAvailable(type) {
+    let storage;
+    try {
+      storage = window[type];
+      const x = "__storage_test__";
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return (
+        e instanceof DOMException &&
+        e.name === "QuotaExceededError" &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        storage &&
+        storage.length !== 0
+      );
+    };
+  };
+
+  static store(masterList) {
+    if (this.storageAvailable("localStorage") === true) {
+      localStorage.setItem("masterList", JSON.stringify(masterList));
+      return true;
+    } else return false;
+  }
+
+  static load() {
+    if (this.storageAvailable("localStorage") === true) {
+      return localStorage.getItem("masterList");
+    } else return false;
+  }
+}
