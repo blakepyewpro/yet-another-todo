7f944c500f427b712442fa280a7ec0450b879b5b
Blake Pugh <bpugh@thumbstopper.com>
2026-04-30 17:53:18 -0400

    feat: add basic load and save to masterList

--- src/items.js
+++ src/items.js
@@ -1,48 +1,76 @@
+import Storage from "./storage.js"
 export class MasterList {
-  constructor(saveData) {
-    this.items = [];
+  constructor() {
+    this.projects = [];
     this.display = [];
 
-    if (saveData) {
-      for (let item in saveData.items) {
-        if (item.itemType == "project") {
-          this.items.push(
-            new Project(item.name, new Date(item.dueDate),)
-          )
-        }
-      } 
+    const saveStr = Storage.load();
+    if (saveStr.projects) {
+      const save = JSON.parse(saveStr);
+      console.log(save);
+      for (const project of save.projects) {
+        this.saveOrUpdate(project);
+      }
+    } else {
+      //TODO: create initial default project and use flag to create empty state
     }
   }
 
-  static processItems (items) {
-    //TODO
-  }
-
-  sortByDueDate() {
-    //TODO
+  saveOrUpdate(item, originalItem) {
+    if (originalItem) {
+      if (originalItem.itemType == "project") {
+        const findProj = (element) => element.name == originalItem.name;
+        const projIndex = this.projects.findIndex(findProj);
+        this.projects[projIndex].name = item.name;
+        Storage.store(this);
+      } else if (originalItem.itemType == "task") {
+        //TODO: find if project assigned changed, if so create new task under new project and delete old; else update existing task
+      }
+    } else {
+      if (item.itemType == "project") {
+        const newProj = new Project(item.name, item.isDefault);
+        if (item.items) newProj.processSaveItems(item.items);
+        this.projects.push(newProj);
+        Storage.store(this);
+      } else if (item.itemType) {
+        const newTask = new Task(item.name, item.notes, item.prio, 
+          item.dueDate, item.project, item.isComplete
+        );
+        const findProj = (element) => element.name == newTask.project;
+        const projIndex = this.projects.findIndex(findProj);
+        this.projects[projIndex].tasks.push(newTask);
+      }
+    }
   }
 }
 
 export class Project {
-  constructor(name, items, isDefault = false) {
+  constructor(name, isDefault = false) {
     this.itemType = "project";
+    this.isDefault = isDefault;
     this.name = name;
-    this.items = items;
-    isDefault = isDefault;
+    this.tasks = [];
   }
 
-  sortByDueDate() {
-    //TODO
+  processSaveItems(saveItems) {
+    for (const task in saveItems) {
+      const newTask = new Task(
+        task.name, task.notes, task.prio, 
+        task.dueDate, task.project, task.isComplete
+      );
+      this.tasks.push(newTask);
+    }
   }
 }
 
 export class Task {
-  constructor(name, description, prio, dueDate, isComplete = false) {
+  constructor(name, notes, prio, dueDate, project, isComplete = false) {
     this.itemType = "task";
     this.name = name;
-    this.description = description;
+    this.notes = notes;
     this.prio = prio;
-    this.dueDate = dueDate;
+    this.dueDate = new Date(dueDate);
+    this.project = project;
     this.isComplete = isComplete;
   }
 }
