258aec7265f9015a988878280ed1660f4fe36af5
Blake Pugh <bpugh@thumbstopper.com>
2026-04-30 17:54:10 -0400

    misc: cleared test code from index

--- src/index.js
+++ src/index.js
@@ -5,16 +5,3 @@ import { MasterList, Project, Task } from "./items.js";
 
 const myUI = new UI();
 myUI.initialize();
-
-let myList = new MasterList();
-let item1 = new Task("item1", "get item1 done", "low", new Date("2026-05-05"));
-let project = new Project("project1", new Date("2026-06-06"), []);
-project.items = [new Task("item2", "do item2", "med", new Date("2026-07-07"))];
-myList.items.push(item1);
-myList.items.push(project);
-
-const myListString = JSON.stringify(myList);
-console.log("String ML: \n" + myListString);
-
-Storage.store(myList);
-console.log("Stored ML: \n" + Storage.load())
